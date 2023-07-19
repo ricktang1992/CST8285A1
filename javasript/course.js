80035ff721d2ed2a0f9d85420ea59fe424b3aaf6
Ziyao Tang <tang0243@algonquinlive.com>
2023-07-18 23:38:59 -0400

    just for u

--- javasript/course.js
+++ javasript/course.js
@@ -95,3 +95,10 @@ function resetCourse(){
         courses[i].style.display = "";
     }
 }
+const inspirations = [
+
+{
+       "quote":"Life isn’t about getting and having, it’s about giving and being.","author":"Kevin Kruse"},
+{
+       "quote":"Whatever the mind of man can conceive and believe, it can achieve.","author":"Napoleon Hill"}
+]
